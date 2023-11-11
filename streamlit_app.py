3bcac8026db4164cfa7b3241c86476f6c9bf2e7b
nguyenviethang193 <150321868+nguyenviethang193@users.noreply.github.com>
2023-11-11 18:23:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+import pandas as pd
+from class1 import User
+# Import other libraries as needed
+st.title("Meal Planner App")
+selected_meal = st.selectbox("Select a Meal", ["Breakfast", "Lunch", "Dinner"])
+selected_date = st.date_input("Select Date")
