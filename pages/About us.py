b530eb5d5092205e01fde03a1b9ed8db6e7ef4fb
Trần Duy Anh <143476110+tranduyanh0112@users.noreply.github.com>
2023-11-25 20:38:17 +0700

    update url

--- pages/About us.py
+++ pages/About us.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title("Team Introduction Page")
 
-    github_raw_url = ""
+    github_raw_url = "https://github.com/nguyenviethang193/Group-3---Recipe-and-Meal-Planner-Application/tree/main/Introduction"
     html_content = st.markdown(github_raw_url, unsafe_allow_html=True)
 
 if __name__ == "__main__":
