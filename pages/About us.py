d10e8504a889652af0c43b90ca99a5f48f7aec2f
Trần Duy Anh <143476110+tranduyanh0112@users.noreply.github.com>
2023-11-25 20:44:10 +0700

    html change

--- pages/About us.py
+++ pages/About us.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
 def main():
-    st.title("Team Introduction Page")
+    # Read HTML file content
+    with open("https://github.com/nguyenviethang193/Group-3---Recipe-and-Meal-Planner-Application/blob/main/Introduction/polaroid.html", "r") as f:
+        html_content = f.read()
 
-    github_raw_url = "https://github.com/nguyenviethang193/Group-3---Recipe-and-Meal-Planner-Application/tree/main/Introduction"
-    html_content = st.markdown(github_raw_url, unsafe_allow_html=True)
+    # Use st.markdown to display HTML content
+    st.markdown(html_content, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
